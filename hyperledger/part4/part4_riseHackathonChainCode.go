d0e783e2cede25f4849e39be41eca21b0bcb8d9a
Chaitanya <chaitanyaamin@gmail.com>
2016-09-22 17:04:04 +0530

    Updated NewEcrire

--- hyperledger/part4/part4_riseHackathonChainCode.go
+++ hyperledger/part4/part4_riseHackathonChainCode.go
@@ -263,7 +263,7 @@ func (t *SimpleChaincode) NewEcrire(stub *shim.ChaincodeStub, args []string) ([]
 	}
 
 	name = args[0] //rename for funsies
-	value = "NewEcrire99:" + args[1]
+	value = "SmartPayTransaction:" + args[1]
 	err = stub.PutState(name, []byte(value)) //write the variable into the chaincode state
 	if err != nil {
 		return nil, err
